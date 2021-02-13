4f53fdaafb31a5ba0a31ac358e5666ac795eb03f
Artem <aprricod@gmail.com>
2021-02-13 13:05:07 +0300

    ngSwitch

--- src/app/comp5/comp5.component.ts
+++ src/app/comp5/comp5.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-comp5',
   templateUrl: './comp5.component.html',
-  styleUrls: ['./comp5.component.css']
+  styleUrls: ['./comp5.component.css'],
 })
 export class Comp5Component implements OnInit {
+  count: number = 1;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  increment() {
+    this.count++;
   }
-
+  decrement() {
+    this.count--;
+  }
 }
