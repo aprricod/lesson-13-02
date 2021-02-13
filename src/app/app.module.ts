0cffc3f3378e1cee8a9a2b5854c703397dace90d
Artem <aprricod@gmail.com>
2021-02-13 12:44:47 +0300

    ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { Comp1Component } from './comp1/comp1.component';
 import { Comp2Component } from './comp2/comp2.component';
 import { Comp3Component } from './comp3/comp3.component';
+import { Comp4Component } from './comp4/comp4.component';
+import { Comp5Component } from './comp5/comp5.component';
 
 @NgModule({
-  declarations: [AppComponent, Comp1Component, Comp2Component, Comp3Component],
+  declarations: [AppComponent, Comp1Component, Comp2Component, Comp3Component, Comp4Component, Comp5Component],
   imports: [BrowserModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent],
